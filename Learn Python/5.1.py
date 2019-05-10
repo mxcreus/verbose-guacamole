e6adea8ec814acd8efaee9b45922e25e95ddc183
mxcreus <sjf1997@hotmail.com>
2019-05-10 11:00:13 +0800

    Add files via upload
    day3

---
+++ Learn Python/5.1.py
@@ -0,0 +1,14 @@
+'''
+def <>(<Required parameters>,<Optional parameter>)
+    <>
+    return <>
+
+'''
+list = ["f","F"]
+def fun(a):
+    list = []
+    list.append(a)
+    print(list)
+    return
+fun("C")
+print(list)
---
+++ Learn Python/5.2.py
@@ -0,0 +1,6 @@
+#lambda function
+#谨慎使用lambda函数
+#一般情况建议使用def
+#除非一些固定搭配
+f = lambda x,y : x+y
+print(f(10,15))
